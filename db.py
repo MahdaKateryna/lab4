a28a6c96970bd3f1c836afecd44373d9b5c455f8
MahdaKateryna <mahda.kateryna@chnu.edu.ua>
2025-05-25 20:11:21 +0300

    3

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import os
 DATABASE_URL = os.getenv("postgresql://lab4_2xl1_user:CajMHHbPMlpsRPjYMlM4LsX8nuYksl6X@dpg-d0pkscmmcj7s73e94lug-a/lab4_2xl1")
 
 def get_connection():
-    return psycopg2.connect(DATABASE_URL)
+    return psycopg2.connect("postgresql://lab4_2xl1_user:CajMHHbPMlpsRPjYMlM4LsX8nuYksl6X@dpg-d0pkscmmcj7s73e94lug-a/lab4_2xl1")
 
 def insert_data(name, email):
     with get_connection() as conn:

9d13f0946ab7f80691422f08a51731d02ef89c80
MahdaKateryna <mahda.kateryna@chnu.edu.ua>
2025-05-26 19:16:51 +0300

    1

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import os
 
 def get_db_connection():
     return psycopg2.connect(
+        url=os.getenv("postgresql://lab4_2xl1_user:CajMHHbPMlpsRPjYMlM4LsX8nuYksl6X@dpg-d0pkscmmcj7s73e94lug-a/lab4_2xl1"),
         dbname=os.getenv("lab4_2xl1"),
         user=os.getenv("lab4_2xl1_user"),
         password=os.getenv("CajMHHbPMlpsRPjYMlM4LsX8nuYksl6X"),

f248f5afcae9691f238ee3a5c7ba72aa05e2c7c1
MahdaKateryna <mahda.kateryna@chnu.edu.ua>
2025-05-26 19:05:15 +0300

    1

--- db.py
+++ db.py
@@ -1,23 +1,11 @@
 import psycopg2
 import os
 
-DATABASE_URL = os.getenv("postgresql://lab4_2xl1_user:CajMHHbPMlpsRPjYMlM4LsX8nuYksl6X@dpg-d0pkscmmcj7s73e94lug-a/lab4_2xl1")
-
-def get_connection():
-    return psycopg2.connect(DATABASE_URL)
-
-def create_users_table():
-    with get_connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute("""
-                CREATE TABLE IF NOT EXISTS users (
-                    id SERIAL PRIMARY KEY,
-                    name TEXT NOT NULL,
-                    email TEXT NOT NULL
-                );
-            """)
-
-def insert_user(name, email):
-    with get_connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute("INSERT INTO users (name, email) VALUES (%s, %s);", (name, email))
+def get_db_connection():
+    return psycopg2.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+    )

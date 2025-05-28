ff44a2b47a5ec679f93547d23e5aec49657a3f71
MahdaKateryna <mahda.kateryna@chnu.edu.ua>
2025-05-28 17:19:33 +0300

    1

--- db.py
+++ db.py
@@ -1,20 +1,35 @@
-import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")  # зчитується з Render або .env
 
 def get_connection():
-    return psycopg2.connect(os.getenv("DATABASE_URL"), cursor_factory=RealDictCursor)
+    return psycopg2.connect(DATABASE_URL)
 
-def create_table():
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            name TEXT NOT NULL,
-            email TEXT NOT NULL
-        );
-    """)
-    conn.commit()
-    cur.close()
-    conn.close()
+def create_users_table():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS users (
+                    id SERIAL PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    email TEXT NOT NULL
+                );
+            """)
+
+def insert_user(name, email):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO users (name, email) VALUES (%s, %s);", (name, email))
+
+
+def get_all_data():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT name, email FROM users")
+            return cur.fetchall()
+
+def insert_data(name, email):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (name, email))
